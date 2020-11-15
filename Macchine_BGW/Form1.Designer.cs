43e6c14c16b4c7e0d045f2e96c18282b1d72cf35
tanchevmarko <marko.tanchev.studenti@isii.it>
2020-11-15 20:56:53 +0100

    Add project files.

---
+++ Macchine_BGW/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Macchine_BGW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ptb3 = new System.Windows.Forms.PictureBox();
+            this.MacchinaRossa = new System.Windows.Forms.PictureBox();
+            this.MacchinaNera = new System.Windows.Forms.PictureBox();
+            this.btnRivincita = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MacchinaRossa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MacchinaNera)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptb3
+            // 
+            this.ptb3.Image = global::Macchine_BGW.Properties.Resources.road_trip_background_10864211;
+            this.ptb3.Location = new System.Drawing.Point(1, 0);
+            this.ptb3.Name = "ptb3";
+            this.ptb3.Size = new System.Drawing.Size(784, 243);
+            this.ptb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb3.TabIndex = 5;
+            this.ptb3.TabStop = false;
+            // 
+            // MacchinaRossa
+            // 
+            this.MacchinaRossa.Image = global::Macchine_BGW.Properties.Resources.macchinarossa;
+            this.MacchinaRossa.Location = new System.Drawing.Point(12, 146);
+            this.MacchinaRossa.Name = "MacchinaRossa";
+            this.MacchinaRossa.Size = new System.Drawing.Size(130, 60);
+            this.MacchinaRossa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.MacchinaRossa.TabIndex = 7;
+            this.MacchinaRossa.TabStop = false;
+            // 
+            // MacchinaNera
+            // 
+            this.MacchinaNera.Image = global::Macchine_BGW.Properties.Resources.macchinabianca;
+            this.MacchinaNera.Location = new System.Drawing.Point(12, 31);
+            this.MacchinaNera.Name = "MacchinaNera";
+            this.MacchinaNera.Size = new System.Drawing.Size(130, 60);
+            this.MacchinaNera.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.MacchinaNera.TabIndex = 6;
+            this.MacchinaNera.TabStop = false;
+            // 
+            // btnRivincita
+            // 
+            this.btnRivincita.Location = new System.Drawing.Point(147, 258);
+            this.btnRivincita.Name = "btnRivincita";
+            this.btnRivincita.Size = new System.Drawing.Size(96, 31);
+            this.btnRivincita.TabIndex = 9;
+            this.btnRivincita.Text = "Rematch";
+            this.btnRivincita.UseVisualStyleBackColor = true;
+            this.btnRivincita.Click += new System.EventHandler(this.btnRivincita_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(13, 258);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(96, 31);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.WorkerReportsProgress = true;
+            this.backgroundWorker2.WorkerSupportsCancellation = true;
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(787, 304);
+            this.Controls.Add(this.btnRivincita);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.MacchinaRossa);
+            this.Controls.Add(this.MacchinaNera);
+            this.Controls.Add(this.ptb3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ptb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MacchinaRossa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MacchinaNera)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptb3;
+        private System.Windows.Forms.PictureBox MacchinaRossa;
+        private System.Windows.Forms.PictureBox MacchinaNera;
+        private System.Windows.Forms.Button btnRivincita;
+        private System.Windows.Forms.Button btnStart;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
---
+++ Macchine_BGW/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Macchine_BGW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Random rnd = new Random();
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker bgw = sender as BackgroundWorker;
+            PictureBox Macchina = (PictureBox)e.Argument;
+
+            while (MacchinaNera.Location.X < ptb3.Width - MacchinaNera.Width + ptb3.Location.X ||
+                MacchinaRossa.Location.X < ptb3.Width - MacchinaRossa.Width + ptb3.Location.X)
+            {
+                if (!bgw.CancellationPending)
+                {
+                    Thread.Sleep(rnd.Next(0, 10));
+                    int n = rnd.Next(0, 5);
+                    bgw.ReportProgress(n, Macchina);
+                }
+            }
+
+            if (!bgw.CancellationPending)
+                e.Result = Macchina;
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            PictureBox Macchina = (PictureBox)e.UserState;
+            int x = Macchina.Location.X + e.ProgressPercentage;
+            Macchina.Location = new Point(x, Macchina.Location.Y);
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (MacchinaNera.Location.X == MacchinaRossa.Location.X)
+                MessageBox.Show("Pareggio");
+            else if (MacchinaNera.Location.X > MacchinaRossa.Location.X)
+                MessageBox.Show("Macchina nera");
+            else if (MacchinaRossa.Location.X > MacchinaNera.Location.X)
+                MessageBox.Show("Macchina rossa");
+        }
+
+
+        private void RipristinaPosizioneMacchine()
+        {
+            MacchinaNera.Location = new Point(0, MacchinaNera.Location.Y);
+            MacchinaRossa.Location = new Point(0, MacchinaRossa.Location.Y);
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync(MacchinaNera);
+            backgroundWorker2.RunWorkerAsync(MacchinaRossa);
+        }
+
+        private void btnRivincita_Click(object sender, EventArgs e)
+        {
+            RipristinaPosizioneMacchine();
+            btnStart_Click(sender, e);
+        }
+    }
+}
